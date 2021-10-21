fc1e65b1d9b49e9a4d73f1fdf440a5430dabc9c3
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2021-10-21 16:44:31 -0700

    Worked on some of the backend and thunk for userProfile

---
+++ react-app/src/store/UserState.js
@@ -0,0 +1,18 @@
+//actions
+
+const FETCH_USER = 'UserState/FETCH_USER';
+const FETCH_ALL_USERS = 'UserState/FETCH_ALL_USERS';
+const UPDATE_USER = 'UserState/UPDATE_USER';
+const REMOVE_USER = 'UserState/REMOVE_USER';
+const CREATE_USER = 'UserState/CREATE_USER';
+
+//action creators
+
+const fetchUser = (userId) => ({
+    
+})
+
+//thunks
+
+
+//reducer
